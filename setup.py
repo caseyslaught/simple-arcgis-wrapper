b2298c04dcb307ff339ca7832b879977cc3ecdc5
Casey Slaught <caseyslaught@gmail.com>
2020-01-08 14:44:15 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -14,19 +14,25 @@ if sys.argv[-1] == 'publish':
 setup(
     name='simple-arcgis-wrapper',
     version=version,
-    packages=['simple_arcgis_wrapper'],
+    author='Casey Slaught',
+    author_email='casey@caracal.cloud',
     description='A simple wrapper for interacting with the ArcGIS Online REST API.',
-    data_files=[('', ['README.md'])],
+    keywords='arcgis gis rest api wrapper',
     license='MIT',
-    author='Caracal',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    url='https://github.com/caracal-cloud/simple-arcgis-wrapper',
+
+    packages=['simple_arcgis_wrapper'],
+    python_requires=">=3",
     install_requires=[
         'requests>=v2.22.0'
     ],
-    python_requires=">=2.7.10",
-    author_email='casey@caracal.cloud',
-    url='https://github.com/caracal-cloud/simple-arcgis-wrapper',
-    keywords='arcgis rest api',
-    classifiers=[],
-    long_description=long_description,
-    long_description_content_type='text/markdown'
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
